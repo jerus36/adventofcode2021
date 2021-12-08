8ae48f58978f83a5bac167ef694b85769892db96
Jeremy King <jking@oilygears.com>
2021-12-07 20:58:04 -0500

    Day2

--- days.py
+++ days.py
@@ -1,7 +1,47 @@
 from day import Day
+from pandas import read_fwf
+
 def load_dater(f):
     return 
+class Day2(Day):
+    def quiz1(self):
+        pos = 0
+        depth = 0
+        for (k,v) in self.data:
+            if k == "forward":
+                pos = pos + v
+            elif k == "down":
+                depth = depth + v
+            elif k == "up":
+                depth = depth - v
+            else:
+                raise IOError("Unknown command")
 
+        return pos * depth
+
+    def quiz2(self):
+        pos = 0
+        depth = 0
+        aim = 0
+        for (k,v) in self.data:
+            if k == "forward":
+                pos = pos + v
+                depth = depth + (aim * v)
+            elif k == "down":
+                aim = aim + v
+            elif k == "up":
+                aim = aim - v
+            else:
+                raise IOError("Unknown command")
+        return pos * depth
+
+    def __init__(self, dfile):
+        super().__init__(dfile)
+        self.data = list(map(lambda x: (x.split()[0],int(x.split()[1])), dfile.readlines()))
+        self.quizzes.append(self.quiz1)
+        self.quizzes.append(self.quiz2)
+       
+        
 class Day1(Day):
     def quiz1(self):
         ret = 0
--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from argparse import ArgumentParser
-from days import Day1
+from days import Day1,Day2
 
 parser = ArgumentParser()
 parser.add_argument("--day", type=int)
 parser.add_argument("--quiz", type=int)
 parser.add_argument("--file", type=str)
-days = [Day1]
+days = [Day1,Day2]
 def main(day, quiz, filename):
     with open(filename) as f:
         day_class = days[day-1](f)
         res = day_class.execute(quiz)
         print(f"Day {day}, Quiz {quiz}, result = {res}")
-    
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
